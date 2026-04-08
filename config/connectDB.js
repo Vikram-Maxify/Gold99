55c71e9ba2d9fe35c4220df2cfd7b766677fdbf5
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-04-08 12:54:22 +0530

    connect db done

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
   host: "195.35.22.205",
-  user: "bhtdemo",
-  password: "bhtdemo",
-  database: "bhtdemo",
+  user: "99gold",
+  password: "E2WKzWZ@%^",
+  database: "99gold",
 });
 
 export default connection;
